dc5359cd8baaac8230d258e129c8cad65edf8b83
Ryan Pickett <piratematic@gmail.com>
2019-08-20 16:03:00 -0600

    Went through the lectures to make DB and site things

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  root 'people#index'
+
+  get '/about', to: 'static_people#about'
+
+  resources :people
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
